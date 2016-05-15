d337059eda925ad7fdf9e62cc6d059c0f72b0655
AkhilNadendla <anadendla@g.ucla.edu>
2016-05-15 00:21:15 -0700

    Fixed some bs

--- app/src/main/java/matthewallenlinsoftware/cs_m117_assassins/login_page.java
+++ app/src/main/java/matthewallenlinsoftware/cs_m117_assassins/login_page.java
@@ -2,10 +2,7 @@ package matthewallenlinsoftware.cs_m117_assassins;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.firebase.client.DataSnapshot;
@@ -20,17 +17,6 @@ public class login_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         Firebase.setAndroidContext(this);
         myFirebaseRef = new Firebase("https://assassinsm1117.firebaseio.com/");
         myFirebaseRef.child("Count").addValueEventListener(new ValueEventListener() {
--- app/src/main/java/matthewallenlinsoftware/cs_m117_assassins/selection_page.java
+++ app/src/main/java/matthewallenlinsoftware/cs_m117_assassins/selection_page.java
@@ -2,13 +2,10 @@ package matthewallenlinsoftware.cs_m117_assassins;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class selection_page extends AppCompatActivity {
 
@@ -16,17 +13,6 @@ public class selection_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
